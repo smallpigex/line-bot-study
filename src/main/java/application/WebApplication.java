c36cc291507eca00cf20ed04797be29ca2d7c663
smallpigex <smallpigex@gmail.com>
2017-04-08 22:53:39 +0800

    add linebot

--- src/main/java/application/WebApplication.java
+++ src/main/java/application/WebApplication.java
@@ -3,12 +3,31 @@ package application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
 @SpringBootApplication
+@LineMessageHandler
 public class WebApplication {
 
 	public static void main(String args[]) {
 		SpringApplication.run(WebApplication.class, args);
 	}
 	
+	@EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+	    
+        System.out.println("event: " + event);
+        return new TextMessage(event.getMessage().getText());
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
 	
 }
