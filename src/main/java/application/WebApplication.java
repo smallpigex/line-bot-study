97f0b0a57d4b549dc52741261e9dc624c36dd613
smallpigex <smallpigex@gmail.com>
2017-04-09 10:23:11 +0800

    test

--- src/main/java/application/WebApplication.java
+++ src/main/java/application/WebApplication.java
@@ -27,40 +27,43 @@ import application.service.PlaceService;
 @SpringBootApplication
 @LineMessageHandler
 public class WebApplication {
-    @Autowired 
-    PlaceService  service;
-	public static void main(String args[]) {
-		SpringApplication.run(WebApplication.class, args);
-	}
-	
-	@EventMapping
+    @Autowired
+    PlaceService service;
+
+    public static void main(String args[]) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+    @EventMapping
     public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-	    
+
         System.out.println("event: " + event);
         String text = event.getMessage().getText();
         String[] textArray = text.split(" ");
-        if(textArray.length == 4 && textArray[0].equals("餓惹")) {
+        if (textArray.length == 4 && textArray[0].equals("餓惹")) {
             Keyword keyord = new Keyword();
             keyord.setPlace(textArray[1]);
             keyord.setType(textArray[2]);
-            if(textArray.length >= 3 && textArray[3] != null) {
+            if (textArray.length >= 3 && textArray[3] != null) {
                 try {
-                    keyord.setRating(Float.valueOf(textArray[3]));                                
+                    keyord.setRating(Float.valueOf(textArray[3]));
                 } catch (NumberFormatException e) {
                     keyord.setRating(4.0f);
                 }
                 List<Place> places = service.find(keyord);
-                 
-                List<CarouselColumn> columns = new ArrayList<CarouselColumn>();   
-                for(Place place : places) {
+
+                List<CarouselColumn> columns = new ArrayList<CarouselColumn>();
+                for (Place place : places) {
                     List<Action> actions = new ArrayList<Action>();
                     URIAction uriAction = new URIAction("abc", place.getGoogleMapUrl());
                     actions.add(uriAction);
-                    CarouselColumn column = new CarouselColumn("yooo", place.getName(), place.getName(), actions);
+                    CarouselColumn column = new CarouselColumn(
+                            "https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/13151586_1333441783337759_4465443793710794474_n.jpg?oh=7cae16f8bab3ac9d0df55a2cfbfec5b8&oe=595300A0",
+                            place.getName(), place.getName(), actions);
                     columns.add(column);
                 }
                 CarouselTemplate template = new CarouselTemplate(columns);
-                
+
                 TemplateMessage templateMsg = new TemplateMessage("Result", template);
                 System.out.println(templateMsg.toString());
                 return templateMsg;
@@ -73,5 +76,5 @@ public class WebApplication {
     public void handleDefaultMessageEvent(Event event) {
         System.out.println("event: " + event);
     }
-	
+
 }
