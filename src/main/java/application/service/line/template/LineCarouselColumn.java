ec3b76bd7051f9de73b7f1e337c864705c90ee2d
hwchu <smallpigex@gmail.com>
2018-01-21 17:44:37 +0800

    add service annotation

--- src/main/java/application/service/line/template/LineCarouselColumn.java
+++ src/main/java/application/service/line/template/LineCarouselColumn.java
@@ -2,12 +2,14 @@ package application.service.line.template;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Service;
 import com.linecorp.bot.model.action.Action;
 import com.linecorp.bot.model.action.URIAction;
 import com.linecorp.bot.model.message.template.CarouselColumn;
 import com.linecorp.bot.model.message.template.CarouselTemplate;
 import application.model.Place;
 
+@Service
 public class LineCarouselColumn implements LineTemplate {
   public CarouselTemplate createLineTemplate(List<Place> places) {
     List<CarouselColumn> columns = new ArrayList<CarouselColumn>();
